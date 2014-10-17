8e67b792b7b66b244be95d49bdfb557be6709aee
Simon Boudrias <admin@simonboudrias.com>
2014-10-17 16:27:51 -0400

    Update #read() to work with latest mem-fs

--- actions/read.js
+++ actions/read.js
@@ -5,7 +5,7 @@ var assert = require('assert');
 module.exports = function (path) {
   var file = this.store.get(path);
 
-  assert(['unexistent', 'deleted'].indexOf(file.state) === -1, path + ' doesn\'t exist');
+  assert(file.contents !== null && file.state !== 'deleted', path + ' doesn\'t exist');
 
   return file.contents.toString();
 };
