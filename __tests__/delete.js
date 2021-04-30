fec287c254a462f8f58246ca0a52f8d564d8df40
Marcelo Shima <marceloshima@gmail.com>
2021-04-30 14:47:42 -0300

    Reimplement state processing (#172)

--- __tests__/delete.js
+++ __tests__/delete.js
@@ -13,42 +13,50 @@ describe('#delete()', () => {
     fs = editor.create(store);
   });
 
-  it('deletes a file', () => {
+  it('deletes existing files', () => {
     const filepath = path.join(__dirname, 'fixtures/file-a.txt');
     fs.delete(filepath);
     expect(fs.read.bind(fs, filepath)).toThrow();
-    expect(fs.store.get(filepath).state).toBe('deleted');
+
+    const file = fs.store.get(filepath);
+    expect(file.contents).toBe(null);
+    expect(file.state).toBe('deleted');
   });
 
-  it('deletes a directory', () => {
+  it('deletes a directory with existing files', () => {
     const dirpath = path.join(__dirname, 'fixtures/nested');
     const nestedFile = path.join(dirpath, 'file.txt');
     fs.delete(dirpath);
-    expect(fs.store.get(nestedFile).state).toBe('deleted');
+
+    const file = fs.store.get(nestedFile);
+    expect(file.contents).toBe(null);
+    expect(file.state).toBe('deleted');
   });
 
   it('deletes new files', () => {
     fs.write('foo', 'foo');
     fs.delete('foo');
-    expect(fs.store.get('foo').state).toBe('deleted');
+
+    const file = fs.store.get('foo');
+    expect(file.contents).toBe(null);
+    expect(file.state).toBe('deleted');
   });
 
   it('deletes new directories', () => {
     fs.write('/test/bar/foo.txt', 'foo');
     fs.delete('/test/bar/');
-    expect(fs.store.get('/test/bar/foo.txt').state).toBe('deleted');
-  });
 
-  it('after delete a file should set isNew flag on write', () => {
-    const filepath = path.join(__dirname, 'fixtures/file-a.txt');
-    fs.delete(filepath);
-    fs.write(filepath, 'foo');
-    expect(fs.store.get(filepath).isNew).toBeTruthy();
+    const file = fs.store.get('/test/bar/foo.txt');
+    expect(file.contents).toBe(null);
+    expect(file.state).toBe('deleted');
   });
 
   it('delete new files if specifying a full path', () => {
     fs.write('bar', 'bar');
     fs.delete(path.resolve('bar'));
-    expect(fs.store.get('bar').state).toBe('deleted');
+
+    const file = fs.store.get('bar');
+    expect(file.contents).toBe(null);
+    expect(file.state).toBe('deleted');
   });
 });
