63ca60ece69923b8b9436e2c132ffcab9bb529c1
Yohan Lasorsa <noda@free.fr>
2021-07-30 19:13:07 +0200

    Add noGlob option (fix #75) (#185)

--- __tests__/copy.js
+++ __tests__/copy.js
@@ -120,7 +120,7 @@ describe('#copy()', () => {
     const outputDir = path.join(__dirname, '../test/output');
     const process = sinon.stub().returnsArg(0);
     fs.copy(path.join(__dirname, '/fixtures/**'), outputDir, {process});
-    sinon.assert.callCount(process, 11); // 9 total files under 'fixtures', not counting folders
+    sinon.assert.callCount(process, 12); // 10 total files under 'fixtures', not counting folders
     expect(fs.read(path.join(outputDir, 'file-a.txt'))).toBe('foo' + os.EOL);
     expect(fs.read(path.join(outputDir, '/nested/file.txt'))).toBe('nested' + os.EOL);
   });
@@ -168,5 +168,11 @@ describe('#copy()', () => {
       expect(newStat.mode).toBe(oldStat.mode);
       done();
     });
+  });
+
+  it('copy with globbing disabled', () => {
+    const newPath = path.join(__dirname, '../test/output', 'file.txt');
+    fs.copy(path.join(__dirname, '/fixtures/file-(specia!-char$).txt'), newPath, {noGlob: true});
+    expect(fs.read(newPath)).toBe('special' + os.EOL);
   });
 });
