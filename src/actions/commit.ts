1d071b05fc8d5fc38e1fda892b87fa208305968c
Marcelo Shima <marceloshima@gmail.com>
2023-10-09 18:18:52 -0300

    use pipeline from mem-fs@4

--- src/actions/commit.ts
+++ src/actions/commit.ts
@@ -1,32 +1,20 @@
-import { promisify } from 'util';
-import { pipeline as _pipeline, PipelineSource } from 'stream';
+import { isFileTransform, type FileTransform, type PipelineOptions } from 'mem-fs';
 import type { MemFsEditor, MemFsEditorFile } from '../index.js';
 
-const pipeline = promisify(_pipeline);
-
-import { createPendingFilesPassthrough, createCommitTransform } from '../transform.js';
+import { createCommitTransform } from '../transform.js';
 import { isFilePending } from '../state.js';
 
-function commit<EditorFile extends MemFsEditorFile>(this: MemFsEditor<EditorFile>, stream?: PipelineSource<any>);
-function commit<EditorFile extends MemFsEditorFile>(
+async function commit<EditorFile extends MemFsEditorFile>(
   this: MemFsEditor<EditorFile>,
-  filters?: any[],
-  stream?: PipelineSource<any>
-);
-function commit<EditorFile extends MemFsEditorFile>(
-  this: MemFsEditor<EditorFile>,
-  filters?: any[] | PipelineSource<any>,
-  stream?: PipelineSource<any>
-) {
-  if (!Array.isArray(filters)) {
-    stream = filters;
-    filters = [];
+  options?: PipelineOptions<EditorFile> | FileTransform<EditorFile>,
+  ...transforms: FileTransform<EditorFile>[]
+): Promise<void> {
+  if (isFileTransform(options)) {
+    transforms = [options, ...transforms];
+    options = undefined;
   }
 
-  stream = stream ?? this.store.stream({ filter: (file: EditorFile) => isFilePending(file) });
-  filters = filters ?? [];
-
-  return pipeline(stream, createPendingFilesPassthrough(), ...filters, createCommitTransform(this));
+  await this.store.pipeline({ filter: isFilePending, ...options }, ...transforms, createCommitTransform());
 }
 
 export default commit;
