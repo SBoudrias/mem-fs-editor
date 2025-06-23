95328f54143bfcfd9e35d7fb51cab17bc0bceb15
Simon Boudrias <admin@simonboudrias.com>
2025-06-23 14:59:52 -0400

    Fix: Flakey test teardown

--- __tests__/dump.spec.ts
+++ __tests__/dump.spec.ts
@@ -1,52 +1,96 @@
-import { describe, beforeEach, it, expect, afterEach } from 'vitest';
-import fs from 'fs';
+import { describe, beforeEach, it, expect } from 'vitest';
 import os from 'os';
 import path from 'path';
 import { create as createMemFs } from 'mem-fs';
 import { type MemFsEditor, MemFsEditorFile, create } from '../src/index.js';
 
 describe('#dump()', () => {
-  const output = path.join(os.tmpdir(), '/mem-fs-editor-test');
-  const subdir = 'foo';
+  let testDir: string;
   let memFs: MemFsEditor;
 
   beforeEach(async () => {
+    const subdir = 'foo';
+
+    testDir = path.join(os.tmpdir(), '/mem-fs-editor-test');
     memFs = create(createMemFs<MemFsEditorFile>());
 
-    memFs.write(path.join(output, subdir, 'committed'), 'committed');
-    memFs.write(path.join(output, subdir, 'committed-delete'), 'committed-delete');
-    memFs.write(path.join(output, subdir, 'committed-changed'), 'orig');
-    memFs.write(path.join(output, subdir, 'committed-delete-changed'), 'orig');
+    memFs.write(path.join(testDir, subdir, 'committed'), 'committed');
+    memFs.write(path.join(testDir, subdir, 'committed-delete'), 'committed-delete');
+    memFs.write(path.join(testDir, subdir, 'committed-changed'), 'orig');
+    memFs.write(path.join(testDir, subdir, 'committed-delete-changed'), 'orig');
     await memFs.commit();
 
-    memFs.delete(path.join(output, subdir, 'committed-delete'));
+    memFs.delete(path.join(testDir, subdir, 'committed-delete'));
     await memFs.commit();
 
-    memFs.write(path.join(output, subdir, 'committed-changed'), 'committed-changed');
-    memFs.write(path.join(output, subdir, 'not-committed'), 'not-committed');
-    memFs.write(path.join(output, subdir, 'not-committed-delete'), 'not-committed-delete');
+    memFs.write(path.join(testDir, subdir, 'committed-changed'), 'committed-changed');
+    memFs.write(path.join(testDir, subdir, 'not-committed'), 'not-committed');
+    memFs.write(path.join(testDir, subdir, 'not-committed-delete'), 'not-committed-delete');
 
-    memFs.delete(path.join(output, subdir, 'committed-delete-changed'));
-    memFs.delete(path.join(output, subdir, 'not-committed-delete'));
-  });
-
-  afterEach(() => {
-    fs.rmSync(output, { recursive: true, force: true });
+    memFs.delete(path.join(testDir, subdir, 'committed-delete-changed'));
+    memFs.delete(path.join(testDir, subdir, 'not-committed-delete'));
   });
 
   it('should match snapshot', () => {
-    expect(memFs.dump(output)).toMatchSnapshot();
+    expect(memFs.dump(testDir)).toMatchInlineSnapshot(`
+      {
+        "foo/committed": {
+          "contents": "committed",
+          "stateCleared": "modified",
+        },
+        "foo/committed-changed": {
+          "contents": "committed-changed",
+          "state": "modified",
+          "stateCleared": "modified",
+        },
+        "foo/committed-delete": {
+          "contents": null,
+          "stateCleared": "deleted",
+        },
+        "foo/committed-delete-changed": {
+          "contents": null,
+          "state": "deleted",
+          "stateCleared": "modified",
+        },
+        "foo/not-committed": {
+          "contents": "not-committed",
+          "state": "modified",
+        },
+        "foo/not-committed-delete": {
+          "contents": null,
+          "state": "deleted",
+        },
+      }
+    `);
   });
 
   describe('with custom filter', () => {
     it('should match snapshot', () => {
-      expect(memFs.dump(output, (file) => file.path.endsWith('not-committed'))).toMatchSnapshot();
+      expect(memFs.dump(testDir, (file) => file.path.endsWith('not-committed'))).toMatchInlineSnapshot(`
+        {
+          "foo/not-committed": {
+            "contents": "not-committed",
+            "state": "modified",
+          },
+        }
+      `);
     });
   });
 
   describe('with a glob pattern', () => {
     it('should return files that matches the pattern and have state or stateCleared', () => {
-      expect(memFs.dump(output, '**/*committed')).toMatchSnapshot();
+      expect(memFs.dump(testDir, '**/*committed')).toMatchInlineSnapshot(`
+        {
+          "foo/committed": {
+            "contents": "committed",
+            "stateCleared": "modified",
+          },
+          "foo/not-committed": {
+            "contents": "not-committed",
+            "state": "modified",
+          },
+        }
+      `);
     });
   });
 });
