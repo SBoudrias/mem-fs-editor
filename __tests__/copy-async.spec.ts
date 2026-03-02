7d3ac728b78edc2f0daf2d5cf2641ea1e3ede263
Marcelo Shima <marceloshima@gmail.com>
2026-03-02 10:56:44 -0300

    chore: synchronize copy/copyAsync tests

--- __tests__/copy-async.spec.ts
+++ __tests__/copy-async.spec.ts
@@ -1,5 +1,5 @@
-import { describe, beforeEach, it, expect, vi } from 'vitest';
-import fs from 'fs';
+import { describe, beforeEach, it, expect, vi, afterEach } from 'vitest';
+import fs from 'fs/promises';
 import os from 'os';
 import path from 'path';
 import { MemFsEditor, MemFsEditorFile, create } from '../src/index.js';
@@ -11,9 +11,16 @@ type CopyAsyncOptions = NonNullable<Parameters<CopyAsyncFunction>[2]>;
 
 describe('#copyAsync()', () => {
   let memFs: MemFsEditor;
+  let outputDir: string;
 
-  beforeEach(() => {
+  beforeEach(async () => {
     memFs = create(createMemFs<MemFsEditorFile>());
+    outputDir = path.join(os.tmpdir(), 'mem-fs-editor', Math.random().toString().split('.')[1]);
+    await fs.mkdir(outputDir, { recursive: true });
+  });
+
+  afterEach(async () => {
+    await fs.rm(outputDir, { recursive: true, force: true });
   });
 
   it('copy file', async () => {
@@ -25,10 +32,14 @@ describe('#copyAsync()', () => {
     expect(memFs.store.get(newPath).state).toBe('modified');
   });
 
-  it('throws when trying to copy from a non-existing file', async () => {
-    const filepath = getFixture('does-not-exits');
-    const newPath = getFixture('../../test/new/path/file.txt');
-    await expect(memFs.copyAsync(filepath, newPath)).rejects.toThrow();
+  it('supports non-vinyl files', async () => {
+    const filepath = getFixture('file-a.txt');
+    const { contents, path } = memFs.store.loadFile(filepath);
+    const file = { contents, path };
+    memFs.store.add(file);
+    const newPath = '/new/path/file.txt';
+    await memFs.copyAsync(filepath, newPath);
+    expect(memFs.store.get(newPath).state).toBe('modified');
   });
 
   describe('using append option', () => {
@@ -64,41 +75,51 @@ describe('#copyAsync()', () => {
     await expect(memFs.copyAsync(filepath, newPath)).rejects.toThrow();
   });
 
+  it('throws when trying to copy from a non-existing file with noGlob option', async () => {
+    const filepath = getFixture('does-not-exits');
+    const newPath = getFixture('../../test/new/path/file.txt');
+    await expect(memFs.copyAsync(filepath, newPath, { noGlob: true })).rejects.toThrow(
+      'Trying to copy from a source that does not exist: ',
+    );
+  });
+
   it('transforms file path and contents using fileTransform', async () => {
     const filepath = getFixture('file-a.txt');
-    const contents = 'some processed contents';
-    const newPath = getFixture('../../test/new/path/file.txt');
     const transformedPath = getFixture('../../test/transformed/path/file.txt');
-    const transformOptions = { someOption: true };
-    await memFs.copyAsync(filepath, newPath, {
-      fileTransform({ destinationPath, sourcePath, contents: fileContents, options }) {
-        expect(destinationPath).toBe(path.resolve(newPath));
-        expect(sourcePath).toBe(filepath);
-        expect(fileContents).toBeInstanceOf(Buffer);
-        expect(options).toMatchObject({ someOption: true });
-        return { path: transformedPath, contents: Buffer.from(contents) };
+    const transformedContent = 'transformed content';
+
+    await memFs.copyAsync(filepath, 'any', {
+      fileTransform() {
+        return { path: transformedPath, contents: Buffer.from(transformedContent) };
       },
-      transformOptions,
     });
-    expect(memFs.read(transformedPath)).toBe(contents);
+
+    expect(memFs.read(transformedPath)).toBe(transformedContent);
+  });
+
+  it('uses default fileTransform when not provided', async () => {
+    const filepath = getFixture('file-a.txt');
+    const initialContents = memFs.read(filepath);
+    const destPath = 'new/path/file.txt';
+
+    await memFs.copyAsync(filepath, destPath);
+
+    expect(memFs.read(destPath)).toBe(initialContents);
   });
 
   it('copy by directory', async () => {
-    const outputDir = getFixture('../../test/output');
     await memFs.copyAsync(getFixture(), outputDir);
     expect(memFs.read(path.join(outputDir, 'file-a.txt'))).toBe('foo' + os.EOL);
     expect(memFs.read(path.join(outputDir, '/nested/file.txt'))).toBe('nested' + os.EOL);
   });
 
   it('copy by globbing', async () => {
-    const outputDir = getFixture('../../test/output');
     await memFs.copyAsync(getFixture('**'), outputDir);
     expect(memFs.read(path.join(outputDir, 'file-a.txt'))).toBe('foo' + os.EOL);
     expect(memFs.read(path.join(outputDir, '/nested/file.txt'))).toBe('nested' + os.EOL);
   });
 
   it('copy by globbing multiple patterns', async () => {
-    const outputDir = getFixture('../../test/output');
     await memFs.copyAsync([getFixture('**'), '!**/*tpl*'], outputDir);
     expect(memFs.read(path.join(outputDir, 'file-a.txt'))).toBe('foo' + os.EOL);
     expect(memFs.read(path.join(outputDir, '/nested/file.txt'))).toBe('nested' + os.EOL);
@@ -108,7 +129,6 @@ describe('#copyAsync()', () => {
   });
 
   it('transforms files when globbing', async () => {
-    const outputDir = getFixture('../../test/output');
     const fileTransform = vi
       .fn<NonNullable<CopyAsyncOptions['fileTransform']>>()
       .mockImplementation(({ destinationPath, contents }) => ({ path: destinationPath, contents }));
@@ -119,7 +139,6 @@ describe('#copyAsync()', () => {
   });
 
   it('accepts directory name with "."', async () => {
-    const outputDir = getFixture('../../test/out.put');
     await memFs.copyAsync(getFixture('**'), outputDir);
     expect(memFs.read(path.join(outputDir, 'file-a.txt'))).toBe('foo' + os.EOL);
     expect(memFs.read(path.join(outputDir, '/nested/file.txt'))).toBe('nested' + os.EOL);
@@ -130,20 +149,35 @@ describe('#copyAsync()', () => {
   });
 
   it('preserve permissions', async () => {
-    const filename = path.join(os.tmpdir(), 'perm.txt');
-    const copyname = path.join(os.tmpdir(), 'copy-perm.txt');
-    fs.writeFileSync(filename, 'foo', { mode: 0o733 });
+    const filename = path.join(outputDir, 'perm.txt');
+    const copyname = path.join(outputDir, 'copy-perm.txt');
+    await fs.writeFile(filename, 'foo', { mode: 0o755, encoding: 'utf-8' });
 
     await memFs.copyAsync(filename, copyname);
 
     await memFs.commit();
-    const oldStat = fs.statSync(filename);
-    const newStat = fs.statSync(copyname);
+    const oldStat = await fs.stat(filename);
+    const newStat = await fs.stat(copyname);
     expect(newStat.mode).toBe(oldStat.mode);
   });
 
+  it('copy with globbing disabled', async () => {
+    const newPath = path.join(outputDir, 'file.txt');
+    await memFs.copyAsync(getFixture('file-(specia!-char$).txt'), newPath, {
+      noGlob: true,
+    });
+    expect(memFs.read(newPath)).toBe('special' + os.EOL);
+  });
+
+  it('copy glob like file when noGlob', async () => {
+    const newPath = path.join(outputDir, 'file.txt');
+    await memFs.copyAsync(getFixture('[file].txt'), newPath, {
+      noGlob: true,
+    });
+    expect(memFs.read(newPath)).toBe('foo' + os.EOL);
+  });
+
   it('accepts fromBasePath', async () => {
-    const outputDir = path.join(os.tmpdir(), 'mem-fs-editor');
     await memFs.copyAsync(['file-a.txt', 'nested/file.txt'], outputDir, {
       fromBasePath: path.join(import.meta.dirname, 'fixtures'),
       noGlob: true,
@@ -153,7 +187,6 @@ describe('#copyAsync()', () => {
   });
 
   it('detects fromBasePath from common prefix', async () => {
-    const outputDir = getFixture('../../test/output');
     await memFs.copyAsync([getFixture('file-a.txt'), getFixture('nested/file.txt')], outputDir, {
       noGlob: true,
     });
@@ -164,8 +197,8 @@ describe('#copyAsync()', () => {
   it('provides correct parameters to fileTransform', async () => {
     const filepath = getFixture('file-tpl.txt');
     const newPath = '/new/path/file.txt';
+    const transformData = { name: 'bar' };
     const transformOptions = { async: true };
-    const transformData = { someData: 123 };
     await memFs.copyAsync(filepath, newPath, {
       fileTransform({ destinationPath, sourcePath, contents, options, data }) {
         // Verify that sourcePath is the original template file
@@ -181,8 +214,8 @@ describe('#copyAsync()', () => {
         // Return unmodified path and content
         return { path: destinationPath, contents };
       },
-      transformOptions,
       transformData,
+      transformOptions,
     });
   });
 });
